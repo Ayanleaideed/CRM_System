1339742e34308177bd87a0441d17b8a6518dc3bf
Ayanleaideed <Ayanleaideed@outlook.com>
2024-05-20 01:04:33 -0500

    vercel hosting path

--- myproject/settings.py
+++ myproject/settings.py
@@ -22,7 +22,13 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'crm-system-sigma.vercel.app',
+    'crm-system-h7ka9aijf-ayanles-projects-abda602c.vercel.app',
+    '.vercel.app',
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
